8b359a1ad99ee31a4a6516036e159a3f7b2ba77c
xxx-Event <s2119@e-mirim.hs.kr>
2023-06-15 13:50:13 +0900

    하이딩과 오버라이딩 차이 예제

---
+++ InheritancePrac/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace InheritancePrac32
+{
+    public class Animal
+    {
+        /// <summary>
+        /// 나이
+        /// </summary>
+        public int Age { get; set; }
+
+        public Animal() { this.Age = 0; }
+
+        public Animal (int age)
+        {
+            this.Age = age;
+        }
+
+        public virtual void Eat()
+        {
+            Console.WriteLine("냠냠 먹습니다.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("쿨쿨 잠을 잡니다.");
+        }
+        
+        private void Private() { }
+        protected void Protected() { }
+        public void Public() { }
+
+        public void TestA()
+        {
+            Private();
+            Protected();
+            Public();
+        }
+
+    }
+}
---
+++ InheritancePrac/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InheritancePrac32
+{
+    public class Dog : Animal
+    {
+        public string Color { get; set; }
+
+        public new void Eat()
+        {
+            Console.WriteLine("허겁지겁 먹습니다.");
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("왈왈 짓습니다.");
+        }
+
+        public void TestB()
+        {
+            //Private();
+            Protected();
+            Public();
+        }
+    }
+}
