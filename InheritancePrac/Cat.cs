0db7d5c318e2a7d2a2bcee5a16e07c8af02a3bbd
xxx-Event <s2119@e-mirim.hs.kr>
2023-06-15 13:51:26 +0900

    abstract 키워드 - 객체 생성 방지

---
+++ InheritancePrac/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InheritancePrac32
+{
+    /// <summary>
+    /// 고양이 클래스
+    /// </summary>
+    public abstract class Cat : Animal
+    {
+        public Cat()
+        {
+        }
+
+        public Cat(int age) : base(age)
+        {
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("자박자박 먹습니다.");
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("냥냥 웁니다.");
+        }
+    }
+}
