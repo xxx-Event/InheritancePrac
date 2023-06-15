b87add1e00c3f987f031621000b6c0e213c5211f
xxx-Event <s2119@e-mirim.hs.kr>
2023-06-15 13:53:08 +0900

    다형성을 활용하기 위한 is와 as 키워드

--- InheritancePrac/Program.cs
+++ InheritancePrac/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InheritancePrac
+namespace InheritancePrac32
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Dog dog = new Dog();
+            dog.Public();
+            //dog.Protected();
+            //dog.Private();
+
+
+            foreach (var item in Animal)
+            {
+                item.Eat();
+                item.Sleep();
+
+                var _dog = item as Dog;
+                if (_dog != null) { _dog.Bark(); }
+
+                var _cat = item as Cat;
+                if (_cat != null) { _cat.Meow(); }
+
+                if (item is Dog)
+                {
+                    ((Dog)item).Bark();
+                }
+                else if (item is Cat)
+                {
+                    ((Cat)item).Meow();
+                }
+
+            }
+
         }
     }
 }
